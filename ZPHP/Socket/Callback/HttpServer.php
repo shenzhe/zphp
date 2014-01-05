33d8e26810ca30e64e7d9a0f068f3a2dcb777f60
shenzhe <shenzhe163@gmail.com>
2014-01-05 15:14:05 +0800

    debug simple http server

--- ZPHP/Socket/Callback/HttpServer.php
+++ ZPHP/Socket/Callback/HttpServer.php
@@ -16,16 +16,16 @@ class HttpServer implements ICallback
     private $_cache;
     public function onStart()
     {
-        //echo 'server start, swoole version: ' . SWOOLE_VERSION . PHP_EOL;
+        echo 'server start, swoole version: ' . SWOOLE_VERSION . PHP_EOL;
     }
 
     public function onConnect()
     {
-        /*
+        
         $params = func_get_args();
         $fd = $params[1];
-        echo "{$fd} connected".PHP_EOL;
-        */
+        //echo "{$fd} connected".PHP_EOL;
+        
     }
 
     /**
@@ -38,37 +38,58 @@ class HttpServer implements ICallback
         $serv = $params[0];
         $fd = $params[1];
         $parser = new HttpParser();
-        $buffer = $this->cache->getBuffer($fd);
-        $nparsed = (int) $this->cache->getBuffer($fd, 'nparsed');
+        $buffer = $this->cache->getBuff($fd);
+        $nparsed = (int) $this->cache->getBuff($fd, 'nparsed');
         $buffer .= $_data;
         $nparsed = $parser->execute($buffer, $nparsed);
         if($parser->hasError()) {
             $serv->close($fd, $params[2]);
-            $this->clearBuff($fd);
+            $this->_clearBuff($fd);
         } elseif ($parser->isFinished()) {
-            $this->clearBuff($fd);
-            var_dump($parser->getEnvironment());
-            $this->sendOne($serv, $fd, 'hello world~');
+            $this->_clearBuff($fd);
+            $result = $this->_route($this->_getData($parser->getEnvironment()));
+            $this->sendOne($serv, $fd, $result);
         } else {
-            $buffer = $this->cache->setBuffer($fd, $buffer);
-            $nparsed = (int) $this->cache->setBuffer($fd, $nparsed, 'nparsed');
+            $buffer = $this->cache->setBuff($fd, $buffer);
+            $nparsed = (int) $this->cache->setBuff($fd, $nparsed, 'nparsed');
         }
     }
 
-    private function clearBuff($fd)
+    private function _getData($data)
     {
-        $this->cache->delBuffer($fd, 'nparsed');
-        $this->cache->delBuffer($fd);
+        $_SERVER = $data;
+        switch ($data['REQUEST_METHOD']) {
+            case 'POST':
+                parse_str($data['QUERY_STRING'].'&'.$data['REQUEST_BODY'], $param);
+                return $param;
+                break;
+            case 'PUT':
+                break;
+            case 'DELETE':
+                break;
+            default:   //GET
+                if(empty($data['QUERY_STRING'])) {
+                    return array();
+                }
+
+                parse_str($data['QUERY_STRING'], $param);
+                return $param;
+                break;
+        }
+    }
+
+    private function _clearBuff($fd)
+    {
+        $this->cache->delBuff($fd, 'nparsed');
+        $this->cache->delBuff($fd);
         return true;
     }
 
     public function onClose()
     {
-        /*
         $params = func_get_args();
-        $fd = $params[1];
-        echo "{$fd} closed".PHP_EOL;
-        */
+        //$fd = $params[1];
+        //echo "{$fd} closed".PHP_EOL;   
         $this->cache->delBuff($params[1]);
     }
 
@@ -92,6 +113,7 @@ class HttpServer implements ICallback
             array(
                 'HTTP/1.1 200 OK',
                 'Content-Type: text/html',
+                'Server:zserver 0.1',
                 'Content-Length: '.strlen($data),
                 '',
                 $data));
@@ -103,9 +125,12 @@ class HttpServer implements ICallback
     private function _route($data)
     {
         try {
-            $server = Protocol\Factory::getInstance(ZConfig::getField('socket', 'protocol', 'Rpc'));
+            $server = Protocol\Factory::getInstance('Http');
             $server->parse($data);
-            $result =  Core\Route::route($server);
+            \ob_start();
+            Core\Route::route($server);
+            $result = \ob_get_contents();
+            \ob_end_clean();
             return $result;
         } catch (\Exception $e) {
             //print_r($e);
@@ -118,7 +143,7 @@ class HttpServer implements ICallback
     {
         $params = func_get_args();
         $worker_id = $params[1];
-        echo "WorkerStart[$worker_id]|pid=" . posix_getpid() . ".\n";
+        //echo "WorkerStart[$worker_id]|pid=" . posix_getpid() . ".\n";
         $config = ZConfig::getField('cache', 'locale');
         $this->cache = ZConn::getInstance($config['adapter'], $config);
 
@@ -126,7 +151,10 @@ class HttpServer implements ICallback
 
     public function onWorkerStop()
     {
+        /*
         $params = func_get_args();
         $worker_id = $params[1];
+        echo "WorkerStop[$worker_id]|pid=" . posix_getpid() . ".\n";
+        */
     }
 }
