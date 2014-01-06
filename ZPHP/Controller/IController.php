4c8ec6347645ba3eb2cb4b500969d558e819cefc
半桶水 <shenzhe163@gmail.com>
2014-01-06 17:46:46 +0800

    add wiki

--- ZPHP/Controller/IController.php
+++ ZPHP/Controller/IController.php
@@ -7,9 +7,18 @@
 namespace ZPHP\Controller;
 interface IController
 {
+	/**
+	 *  设置服务
+	 */
     function setServer($server);
 
+    /**
+	 * 业务逻辑开始前执行
+	 */
     function _before();
 
+    /**
+	 * 业务逻辑结束后执行
+	 */
     function _after();
 }
--- ZPHP/Protocol/IProtocol.php
+++ ZPHP/Protocol/IProtocol.php
@@ -9,13 +9,28 @@ namespace ZPHP\Protocol;
 
 interface IProtocol
 {
+	/**
+	 *  解析不同方式传来的数据成统一的格式
+	 */
     public function parse($data);
 
+    /**
+	 *  获取当前业务的action名
+	 */
     public function getAction();
 
+    /**
+	 *  获取当前业务的methoed名
+	 */
     public function getMethod();
 
+    /**
+	 *  获取当前业务的参数
+	 */
     public function getParams();
 
+    /**
+	 *  显示当前业务逻辑处理后的结果数据
+	 */
     public function display($model);
 }
