e6d225d18745dbd842cec5408ec872151cdaea9e
半桶水 <shenzhe163@gmail.com>
2014-02-26 10:19:13 +0800

    debug task config, add php cache support

---
+++ ZPHP/Cache/Adapter/Php.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * User: shenzhe
+ * Date: 13-6-17
+ */
+
+
+namespace ZPHP\Cache\Adapter;
+use ZPHP\Cache\ICache;
+
+class Php implements ICache
+{
+    private $_cache = array();
+
+    public function __construct($config = null)
+    {
+        
+    }
+
+    public function enable()
+    {
+        return true;
+    }
+
+    public function selectDb($db)
+    {
+        return true;
+    }
+
+    public function add($key, $value, $timeOut = 0)
+    {
+        if (!empty(isset($this->_cache[$key]))) {
+            throw new Exception("{$key} exitst");
+        }
+
+        return $this->_cache[$key] = array(
+            $value, (time() + $timeOut)
+        )
+    }
+
+    public function set($key, $value, $timeOut = 0)
+    {
+        return $this->_cache[$key] = array(
+            $value, (time() + $timeOut)
+        )
+    }
+
+    public function get($key)
+    {
+        if(empty($this->_cache[$key])) {
+            return null;
+        }
+
+        if(!empty($this->_cache[$key][1]) && $this->_cache[$key][1] <= time()) { //过期了
+            unset($this->_cache[$key]); 
+            return null;
+        }
+        return $this->_cache[$key][0];
+    }
+
+    public function delete($key)
+    {
+        return unset($this->_cache[$key]);
+    }
+
+    public function increment($key, $step = 1)
+    {
+        if(!empty($this->_cache[$key][0])) {
+            if (!\is_numeric($this->_cache[$key][0])) {
+                throw new Exception("value no numeric");
+            }
+            $this->_cache[$key][0] += $step;
+        } else {
+
+            $this->_cache[$key] = array(
+                $step, 0
+            );
+        }
+        return $this->_cache[$key][0];
+    }
+
+    public function decrement($key, $step = 1)
+    {
+        if(!empty($this->_cache[$key][0])) {
+            if (!\is_numeric($this->_cache[$key][0])) {
+                throw new Exception("value no numeric");
+            }
+            $this->_cache[$key][0] -= $step;
+        } else {
+
+            $this->_cache[$key] = array(
+                0-$step, 0
+            );
+        }
+        return $this->_cache[$key][0];
+    }
+
+    public function clear()
+    {
+        return $this->_cache = array();
+    }
+}
--- ZPHP/Socket/Adapter/Swoole.php
+++ ZPHP/Socket/Adapter/Swoole.php
@@ -28,13 +28,13 @@ class Swoole implements IServer
             'poll_thread_num' => empty($config['poll_thread_num']) ? 2 : $config['poll_thread_num'], //reactor thread num
             'writer_num' => empty($config['writer_num']) ? 2 : $config['writer_num'], //writer thread num
             'worker_num' => empty($config['worker_num']) ? 2 : $config['worker_num'], //worker process num
-            'task_worker_num' => empty($config['task_worker_num']) ? 2 : $config['task_worker_num'], //task worker process num
+            'task_worker_num' => empty($config['task_worker_num']) ? 0 : $config['task_worker_num'], //task worker process num
             'backlog' => empty($config['backlog']) ? 128 : $config['backlog'], //listen backlog));
             'max_request' => empty($config['max_request']) ? 1000 : $config['max_request'],
             'max_conn' => empty($config['max_conn']) ? 100000 : $config['max_conn'],
             'dispatch_mode' => empty($config['dispatch_mode']) ? 2 : $config['dispatch_mode'],
-            'heartbeat_idle_time' => empty($config['heartbeat_idle_time']) ? 30 : $config['heartbeat_idle_time'],
-            'heartbeat_check_interval' => empty($config['heartbeat_check_interval']) ? 30 : $config['heartbeat_check_interval'],
+            'heartbeat_idle_time' => empty($config['heartbeat_idle_time']) ? 0 : $config['heartbeat_idle_time'],
+            'heartbeat_check_interval' => empty($config['heartbeat_check_interval']) ? 0 : $config['heartbeat_check_interval'],
             'log_file' => empty($config['log_file']) ? '/tmp/swoole.log' : $config['log_file'],
             'daemonize' => empty($config['daemonize']) ? 0 : 1,
         ));
