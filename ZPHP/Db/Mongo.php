03e1a8c496533ff097b6524fb675b13bb12f22aa
shenzhe <shenzhe163@gmail.com>
2014-05-09 11:28:51 +0800

    debug mongo

--- ZPHP/Db/Mongo.php
+++ ZPHP/Db/Mongo.php
@@ -29,7 +29,7 @@ class Mongo
             if(!empty($config['options'])) {
                 $options = $config['options'];
             }
-            $this->mongo = new Mongo($config['dsn'], $options);
+            $this->mongo = new \Mongo($config['dsn'], $options);
             
         }
     }
