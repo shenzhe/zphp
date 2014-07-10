970a854c21d73c8cb1198b8575ca64e6de82bb82
半桶水 <shenzhe@shenzhedembp-2.lan>
2014-07-10 18:54:19 +0800

    debug config

--- ZPHP/Socket/Adapter/Swoole.php
+++ ZPHP/Socket/Adapter/Swoole.php
@@ -23,21 +23,7 @@ class Swoole implements IServer
         }
         $this->config = $config;
         $this->serv = new \swoole_server($config['host'], $config['port'], $config['work_mode']);
-        $this->serv->set(array(
-            'timeout' => empty($config['timeout']) ? 2 : $config['timeout'], //select and epoll_wait timeout.
-            'poll_thread_num' => empty($config['poll_thread_num']) ? 2 : $config['poll_thread_num'], //reactor thread num
-            'writer_num' => empty($config['writer_num']) ? 2 : $config['writer_num'], //writer thread num
-            'worker_num' => empty($config['worker_num']) ? 2 : $config['worker_num'], //worker process num
-            'task_worker_num' => empty($config['task_worker_num']) ? 0 : $config['task_worker_num'], //task worker process num
-            'backlog' => empty($config['backlog']) ? 128 : $config['backlog'], //listen backlog));
-            'max_request' => empty($config['max_request']) ? 1000 : $config['max_request'],
-            'max_conn' => empty($config['max_conn']) ? 100000 : $config['max_conn'],
-            'dispatch_mode' => empty($config['dispatch_mode']) ? 2 : $config['dispatch_mode'],
-            'heartbeat_idle_time' => empty($config['heartbeat_idle_time']) ? 0 : $config['heartbeat_idle_time'],
-            'heartbeat_check_interval' => empty($config['heartbeat_check_interval']) ? 0 : $config['heartbeat_check_interval'],
-            'log_file' => empty($config['log_file']) ? '/tmp/swoole.log' : $config['log_file'],
-            'daemonize' => empty($config['daemonize']) ? 0 : 1,
-        ));
+        $this->serv->set($config);
     }
 
     public function setClient($client)
