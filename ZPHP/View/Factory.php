968d8cec11c7315fd85ebf89c551438cdd005b43
shenzhe <shenzhe163@gmail.com>
2015-01-14 19:39:28 +0800

    view

--- ZPHP/View/Factory.php
+++ ZPHP/View/Factory.php
@@ -11,7 +11,7 @@ class Factory
 {
     public static function getInstance($adapter = 'Json')
     {
-        if(is_file(__DIR__.DS.'Adapter'.DS.$adapter.'php')) {
+        if(is_file(__DIR__.DS.'Adapter'.DS.$adapter.'.php')) {
             $className = __NAMESPACE__ . "\\Adapter\\{$adapter}";
         } else {
             $className = $adapter; 
