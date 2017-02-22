e9a306c12347be707144ffe4566df32566dab3fe
shenzhe <shenzhe163@gmail.com>
2017-02-22 15:57:30 +0800

    ant view 优化

--- ZPHP/View/Adapter/Ant.php
+++ ZPHP/View/Adapter/Ant.php
@@ -12,15 +12,14 @@ use ZPHP\Protocol\Request;
 use ZPHP\Protocol\Response;
 use ZPHP\View\Base;
 use ZPHP\Core\Config;
-use ZPHP\Common\MessagePacker;
 
 class Ant extends Base
 {
     public function display()
     {
-        $data = \json_encode($this->model, JSON_UNESCAPED_UNICODE);
-
+        //http服务，请求头需处理
         if (Request::isHttp()) {
+            $data = \json_encode($this->model, JSON_UNESCAPED_UNICODE);
             Response::sendHttpHeader();
             $params = Request::getParams();
             $key = Config::getField('project', 'jsonp', 'jsoncallback');
@@ -34,19 +33,19 @@ class Ant extends Base
             if (Request::isLongServer()) {
                 return $data;
             }
+
+            echo $data;
+            return null;
         }
 
+        //长驻服务，数据直接返回
         if (Request::isLongServer()) {
-            $message = new MessagePacker();
-            $header = Response::getHeaders();
-            $message->writeString(json_encode($header));
-            $message->writeString($data);
-            return $message->getData();
+            return \json_encode([Response::getHeaders(), $this->model], JSON_UNESCAPED_UNICODE);
         }
-        echo $data;
+
+        //正常的php服务，直接echo
+        echo \json_encode($this->model, JSON_UNESCAPED_UNICODE);
         return null;
 
     }
-
-
 }
