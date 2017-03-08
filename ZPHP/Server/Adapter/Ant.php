c3ca7e9d8ba65099533506340b106fd04f0ef52f
shenzhe <shenzhe163@gmail.com>
2017-03-08 18:43:14 +0800

    自动适别server type对应的client class

--- ZPHP/Server/Adapter/Ant.php
+++ ZPHP/Server/Adapter/Ant.php
@@ -9,6 +9,7 @@ namespace ZPHP\Server\Adapter;
 
 use ZPHP\Protocol\Request;
 use ZPHP\Protocol\Factory as ZProtocol;
+use ZPHP\Socket\Adapter\Swoole;
 use ZPHP\Socket\Factory as SFactory;
 use ZPHP\Core\Config;
 use ZPHP\Core\Factory as CFactory;
@@ -24,7 +25,22 @@ class Ant implements IServer
         }
         $socket = SFactory::getInstance(Config::getField('socket', 'adapter', 'Swoole'), $config);
         if (method_exists($socket, 'setClient')) {
-            $client = CFactory::getInstance(Config::getField('socket', 'client_class', 'socket\Tcp'));
+            $type = Config::getField('socket', 'server_type');
+            $clientClass = 'socket\Tcp';
+            switch ($type) {
+                case Swoole::TYPE_WEBSOCKET:
+                case Swoole::TYPE_WEBSOCKETS:
+                    $clientClass = 'socket\WebSocket';
+                    break;
+                case Swoole::TYPE_HTTP:
+                case Swoole::TYPE_HTTPS:
+                    $clientClass = 'socket\Http';
+                    break;
+                case Swoole::TYPE_UDP:
+                    $clientClass = 'socket\Udp';
+                    break;
+            }
+            $client = CFactory::getInstance(Config::getField('socket', 'client_class', $clientClass));
             $socket->setClient($client);
         }
         Request::setServer(ZProtocol::getInstance(Config::getField('socket', 'protocol', 'Ant')));
